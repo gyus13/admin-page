5f5255a28b853426093092fe1e1f10551f64471f
gyus13 <gyusss13@naver.com>
2022-09-12 23:07:02 +0900

    Feat: Coupon Entity 작성

---
+++ src/entity/coupon.entity.ts
@@ -0,0 +1,18 @@
+import { Entity, Column, ManyToOne } from 'typeorm';
+import { CommonEntity } from './common.entity';
+import { ApiProperty } from '@nestjs/swagger';
+
+@Entity('COUPON')
+export class CouponEntity extends CommonEntity {
+  @ApiProperty()
+  @Column()
+  type: string;
+
+  @ApiProperty()
+  @Column()
+  percent: string;
+
+  @ApiProperty()
+  @Column()
+  price: string;
+}
